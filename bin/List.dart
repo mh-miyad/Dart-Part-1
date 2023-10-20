78bc5564911be8df301a253872eb6efa59e5be75
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-10-20 20:21:21 +0600

    Add new List method basic

---
+++ bin/List.dart
@@ -0,0 +1,9 @@
+void main() {
+  const city = ['A', 'B', 'C', 'D', 'E'];
+  //city.add()
+  //city.insert()   here i can add item in the last position of the List
+  //city.insertAll()   here i can add item in the last position of the List
+  //city.addAll()   here i can add item in the last position of the List
+
+  print(city);
+}
