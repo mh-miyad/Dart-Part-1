47eedc3fcd4a1239205395a9cb9ed71d9f651ada
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-10-20 19:47:58 +0600

    first commit

---
+++ bin/besic.dart
@@ -0,0 +1,11 @@
+//?  here i print  my first language
+// !void main() {
+//   print("hello world!");
+// }
+//?  here i print  my first language
+
+void main() {
+  var a = 1212;
+  var b = 1212;
+  print(a + b);
+}
