2a6c5275a4a136f1862df3c19ee39fe6a9d83db5
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-10-20 20:14:15 +0600

    add some basic of dart

--- bin/besic.dart
+++ bin/besic.dart
@@ -1,11 +1,30 @@
-//?  here i print  my first language
-// !void main() {
+void main() {
+  //?  here i print  my first language
+
 //   print("hello world!");
-// }
+
 //?  here i print  my first language
 
-void main() {
-  var a = 1212;
-  var b = 1212;
-  print(a + b);
+//  Dart Data Types
+
+//   // Number
+//   int x = 12;
+//   double y = 45.03;
+//   print(x + y);
+
+  ///! here List start and this is list and it called array in other Javascript language
+
+//   var city = ["12", 21, 132, 231, 451, 71, 61, 51, 41, 0.90, 10];
+//   print(city[9]);
+
+  ///! here Map start and this is map and it called object in other Javascript language
+//   var person = {
+//     "First-name": "MH ",
+//     "last-name": "Miyad",
+//   };
+//! Never change value so i can use const and final
+
+  const a = [1, 2, 3, 4, 5, 6, 7];
+  var ab = a.single;
+  print(ab);
 }
